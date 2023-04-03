76d119ad4bedd735cb32d442d30a486b3d91e858
Franco Lepe <Francolepe169@gmail.com>
2023-04-03 13:39:54 -0400

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
+#num_list = range(1, 10, 2)
+
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n%2 == 0]
+
+    
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+
+
+
+
+
+
+
+
+
+
+
+
+# def return_evens(num_list):
+#     return [n for n in num_list if n%2 == 0]
+
+# def make_exclamation(sentence_list):
+#     return [sentence + "!" for sentence in sentence_list]
